e083f0b32e062ad1529ca3148febe4a4b3bbf2bf
MasterNguyeenx <53168850+MasterNguyeenx@users.noreply.github.com>
2019-08-06 17:07:30 +0700

    Update Assignment 7(3).cpp

--- Assignment 7(3).cpp
+++ Assignment 7(3).cpp
@@ -1,39 +1,41 @@
 #include<stdio.h>
 
-int main() {
-   int a[4][3],i,j,temp;
+int main(){
+	int a[10][10], dong, cot, i, j;
+	int b[10][10], m = 1, n;
 
-   printf("\nNhap ma tran:\n");
-   for (i = 0; i < 4; i++) {
-      for (j = 0; j < 3; j++) {
-         scanf("%d", &a[i][j]);
-      }
-   }
+	printf("Nhap so dong, so cot: ");
+	scanf("%d%d",&dong,&cot);
 
-   printf("\nMa tran vua nhap la: \n");
-   for (i = 0; i < 4; i++) {
-      for (j = 0; j < 3; j++) {
-         printf("%5d", a[i][j]);
-      }
-      printf("\n");
-   }
+	for(i=1;i<=dong;i++){
+		for(j=1;j<=cot;j++){
+			printf("\nNhap vao phan tu a[%d][%d]: ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
 
-   /* Tim ma tran chuyen vi */
-   for (i = 1; i < 4; i++) {
-      for (j = 0; j < 3; j++) {
-         temp = a[i][j];
-         a[i][j] = a[j][i];
-         a[j][i] = temp;
-      }
-   }
+	printf("Ma tran da nhap la:\n");
+	for(i=1; i<=dong; i++){
+		for(j=1; j<=cot; j++){
+			printf("%5d",a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(j = 1; j <= cot; j ++) {
+		n = 1;
+		for(i = 1; i <= dong; i ++) {
+			b[m][n] = a[i][j];
+			n ++;
+		}
+		m ++;
+	}
 
-   printf("\nMa tran chuyen vi cua ma tran da cho la:\n");
-   for (i = 0; i < 4; i++) {
-      for (j = 0; j < 3; j++) {
-         printf("%5d", a[i][j]);
-      }
-      printf("\n");
-   }
-
-   return 0;
+	printf("Ma tran chuyen vi : \n");
+	for(i = 1; i <= cot; i ++){
+		for(j = 1; j <= dong; j ++){
+			printf("%5d", b[i][j]);
+		}
+		printf("\n");
+	}
 }
